d85abb9398116fea0d17e73fb11693c030175260
DIGITAL MINE <digitalmine23@gmail.com>
2020-06-29 15:24:14 +0100

    Update steem_active_users.py
    changed the library from steem to beem in order to be active for both steem and hive blockchains

--- steem_active_users.py
+++ steem_active_users.py
@@ -1,53 +1,71 @@
-from steem import Steem
-from steem.blockchain import Blockchain
+from beem import Steem
+from beem.blockchain import Blockchain
+from beem.nodelist import NodeList
 import time
 import re
 
-b=Blockchain()
-s=Steem()
 
-def hour_active():
-	bl_num=int(b.get_current_block_num())
-	bl_num_=bl_num-1250
-	bl=(str(s.get_blocks_range(bl_num_,bl_num)))
+def hour_active(chain):
+        nodelist = NodeList()
+        nodelist.update_nodes()
+        if chain=='steem':
+                s = Steem(node=nodelist.get_steem_nodes())
+        else:
+                s = Steem(node=nodelist.get_hive_nodes())
+        b=Blockchain(s)
+        bl_=[]
+        bl_num=int(b.get_current_block_num())
+        bl_num_=bl_num-1250
+        bl=b.blocks(bl_num_,bl_num)
+        for i in bl:
+                bl_.append(i['transactions'])
 
 	
-	x=('follower','account','voter','from','author')
-	account=[]
-	new=[]
-	for i in x:
-		acc=re.findall('"'+i+'":"(.+?)"',str(bl))
-		for l in acc:
-			if l not in account:
-				account.append(l)
-		
-	file=open('active_acc.txt','r')
-	old=file.readlines()
-	file.close()
-	file=open('active_acc.txt','a')
+        x=('follower','account','voter','from','author')
+        account=[]
+        new=[]
+        for i in x:
+                acc=re.findall('"'+i+'":"(.+?)"',str(bl_))
+                for l in acc:
+                        if l not in account:
+                                account.append(l)
+        if chain=='steem':
+                file=open('active_acc_steem.txt','r')
+                old=file.readlines()
+                file.close()
+                file=open('active_acc_steem.txt','a')
 
-	for i in account:
-		if i+'\n' not in old:
-			file.write(str(i)+'\n')
-			new.append(i)
-	file.close()
-	print ('one hour accounts',len(account))
-	print ('one hour brand new',len(new))
-	print ('total ACTIVE accounts',len(old))
+                for i in account:
+                        if i+'\n' not in old:
+                                file.write(str(i)+'\n')
+                                new.append(i)
+                file.close()
+        else:
+                file=open('active_acc_hive.txt','r')
+                old=file.readlines()
+                file.close()
+                file=open('active_acc_hive.txt','a')
+                for i in account:
+                        if i+'\n' not in old:
+                                file.write(str(i)+'\n')
+                                new.append(i)
+                file.close()
+                
+        print ('one hour accounts '+chain,len(account))
+        print ('one hour brand new '+chain,len(new))
+        print ('total ACTIVE accounts '+chain,len(old))
 	
 
 
 counter=1
 while True:
-	print (counter)
-	try:
-		hour_active()
-	except Exception :
-		pass
-		print ('FAILED')
-	counter+=1
-	print ('__________________________')
-	time.sleep(3600)
-
-	
-	
+        print (counter)
+        try:
+        	hour_active('steem')
+        	hour_active('hive')
+        except Exception as e:
+        	print (e)
+        	pass
+        counter+=1
+        print ('__________________________')
+        time.sleep(3580)
